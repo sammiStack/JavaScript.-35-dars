3f5b0848d13248fa1836fc78c8974ea13c0491b4
sammibadriddinov <76088408+sammibadriddinov@users.noreply.github.com>
2021-09-27 21:56:30 +0200

    Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loader = document.querySelector(".loader");
+  setTimeout(function () {
+    loader.style.opacity = "0";
+    setTimeout(function () {
+      loader.style.display = "none";
+    }, 2000);
+  }, 3500);
+});
